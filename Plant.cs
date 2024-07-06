ad3d50b9040d173305b842cd7f4bc1b952a90361
Ross Morgan <ross.morgan933@gmail.com>
2024-07-06 13:36:59 -0500

    Add project files.

---
+++ Plant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Plant
+{
+    public string Species { get; set; }
+    public int LightNeeds { get; set; }
+    public decimal AskingPrice { get; set; }
+    public string City { get; set; }
+    public int ZIP { get; set; }
+    public bool Sold { get; set; }
+}
